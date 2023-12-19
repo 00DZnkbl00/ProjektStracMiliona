7bd729e7bf4fe5a7e4be62dd9ce36efbce2864ca
“00DZnkbl00” <dawidzdrojewski12345678@gmail.com>
2023-12-19 09:51:40 +0100

    Added some questions

--- StracMiliona/src/main/java/pl/dawidzjava/stracmiliona/SerializeQuestion.java
+++ StracMiliona/src/main/java/pl/dawidzjava/stracmiliona/SerializeQuestion.java
@@ -10,11 +10,46 @@ public class SerializeQuestion {
 
     public static void main(String[] args) {
 
-        Question question = new Question("Test?", "NO", "NO", "YES", "NO", 0, 3);
-        addQuestion(question);
-        addQuestion(new Question("Test!", "YES", "NO", "NO", "NO", 0, 1));
-        addQuestion(new Question("Test!?", "NO", "YES", "NO", "NO", 0, 2));
-        addQuestion(new Question("Test?!", "NO", "NO", "NO", "YES", 0, 4));
+        addQuestion(new Question("Który kraj leży na Półwyspie Apenińskim?"
+                , "Francja", "Włochy", "Hiszpania"
+                , "Grecja", 0, 2));
+
+        addQuestion(new Question("Jakie morze oddziela Grecję od Turcji?"
+                , "Morze Adriatyckie", "Morze Czarne", "Morze Egejskie"
+                , "Morze Jońskie", 0, 3));
+
+        addQuestion(new Question("W którym kraju znajduje się Machu Picchu?"
+                , "Brazylia", "Peru", "Meksyk"
+                , "Kolumbia", 0, 2));
+
+        addQuestion(new Question("Jaka to góra, najwyższy szczyt Afryki?"
+                , "Mont Blanc", "Matterhorn", "Kilimandżaro"
+                , "Góra Elbrus", 0, 3));
+
+        addQuestion(new Question("Które państwo jest największe pod względem powierzchni na świecie?"
+                , "Rosja", "Stany Zjednoczone", "Kanada"
+                , "Chiny", 0, 1));
+
+        addQuestion(new Question("Na którym kontynencie leży Australia?"
+                , "Azja", "Europa", "Ameryka Południowa"
+                , "Australia", 0, 4));
+
+        addQuestion(new Question("Które miasto jest stolicą Japonii?"
+                , "Pekin", "Tokio", "Seul"
+                , "Bangkok", 0, 2));
+
+        addQuestion(new Question("Jaka rzeka przepływa przez Paryż?"
+                , "Tamiza", "Sekwana", "Dunaj"
+                , "Ren", 0, 2));
+
+        addQuestion(new Question("W którym kraju znajduje się Wielki Kanion Kolorado?"
+                , "Kanada", "Meksyk", "Stany Zjednoczone"
+                , "Australia", 0, 3));
+
+        addQuestion(new Question("Gdzie leży Złota Brama w San Francisco?"
+                , "Nowy Jork", "Los Angeles", "San Francisco"
+                , "Miami", 0, 3));
+
     }
 
     //USED: https://reintech.io/blog/java-serialization-saving-restoring-objects-to-from-disk
